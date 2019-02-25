38937758f8aa1228fc58b8a748ebc0647bcf4751
Arun pandey <arun1186.cse16@chitkara.edu.in>
2019-02-25 09:22:35 +0000

    Comment functionality added

---
+++ YelpCamp/version4/app.js
@@ -0,0 +1,138 @@
+
+var express     =   require("express"),
+    app         =   express(),
+    bodyParser  =   require("body-parser"),
+    mongoose    =   require("mongoose"),
+    Campground  =   require("./models/campground"),
+    seedDB      =   require("./seeds"),
+    Comment     =   require("./models/comment");
+
+mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine" , "ejs");
+
+
+seedDB();
+
+
+/***** INDEX ********************* main landing page *******************************************/
+app.get("/" , (req,res)=>{
+   res.redirect("/campground"); 
+});
+
+
+app.get("/campground" , (req , res)=>{
+    
+    //--------------- fetching campground from db
+    Campground.find({} , function(err , allCampGrounds){
+        
+        if(err)
+        {
+            console.log(err);
+        }else{
+            res.render("campgrounds/index",{campGrounds : allCampGrounds});
+        }
+         
+    });
+});
+
+/****** NEW post ********************** move to add page ******************************************/
+app.get("/campground/new" , (req,res)=>{
+    res.render("campgrounds/new");
+     
+});
+
+
+
+/***** ADD NEW CAMPGROUND *********** post request to add new camp ground ********************************/
+app.post("/campground", (req,res)=>{
+    
+    var newCampground = {
+        name : req.body.name,
+        image : req.body.image,
+        description: req.body.description
+    };
+    //---- creating new campground or inserting 
+    Campground.create(newCampground ,function(err , newlyCreated){
+        
+        if(err){
+            console.log(err);
+        }else{
+            //console.log("New Campground created");
+            //console.log(newlyCreated); //just checking
+            //------------- redirecting to camground page
+            res.redirect("/campground");
+        }
+        
+    });
+   
+});
+
+// ------------------- show templates--------
+app.get("/campground/:id" , (req, res)=>{
+      
+      Campground.findById(req.params.id).populate("comments").exec( (err, foundCampground)=>{
+          if(err)
+          {
+              console.log(err);
+          }else{
+              res.render("campgrounds/show", { campground : foundCampground});
+          }
+          
+      });
+      
+});
+
+
+//------------------ comments route ------------
+
+
+app.get("/campground/:id/comments/new" , function(req , res){
+    
+    Campground.findById(req.params.id , function(err , campground){
+       
+       if(err) {
+           console.log(err);
+       } else {
+           res.render("comments/new" , {campground : campground});
+       }
+        
+    });
+});
+
+app.post("/campground/:id/comments" , function(req, res){
+    
+    Comment.create(req.body.comments , function(err , comment){
+        if (err) {
+            console.log(err);
+        } else {
+            Campground.findById(req.params.id , function(err , foundCampground){
+                if(err) {
+                    console.log(err);
+                } else {
+                    
+                    foundCampground.comments.push(comment);
+                        foundCampground.save();
+                        
+                        res.redirect("/campground/"+req.params.id);
+                }
+                
+            });
+            
+        }
+        
+    });
+    
+});
+
+
+
+
+/********************************* for ALL route *******************************************/
+app.get("*" , (req,res)=>{
+   res.redirect("/campground"); 
+});
+
+app.listen(process.env.PORT , process.env.IP , ()=>{
+    console.log("Server listening!!");
+});
---
+++ YelpCamp/version4/seeds.js
@@ -0,0 +1,69 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   =  require("./models/comment");
+
+var data = [
+       {
+           name : "Mountain",
+           image : "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+           description : "jai"
+       },
+       {
+           name : "river",
+           image : "https://images.unsplash.com/photo-1505232530843-7e94d7faac73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+           description : "blha "
+       },
+       {
+           name : "Fores",
+           image : "https://images.unsplash.com/photo-1475564481606-0f9f5d97c047?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+           description : "anne de anne"
+       }
+    ];
+
+
+
+
+function seedDB() {
+        
+        Campground.remove({} , function(err){
+        if(err) {
+            console.log(err);
+        }
+        console.log("remove all campGrounds");
+        
+        // Adding new Campground
+        data.forEach(function(seed){
+            Campground.create(seed , function(err , camground){
+                
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log("Campground ground added");
+                    
+                        //Comment ----------------
+                        Comment.create({
+                            text : "It is greate place to relax your mind",
+                            author : "Arun"
+                        }, function(err , comment){
+                             if (err) {
+                                 console.log(err);
+                             } else {
+                                 console.log("Comment added");
+                                 camground.comments.push(comment);
+                                 camground.save(); // save
+                                 
+                             }
+                        });
+                }
+           });
+            
+            
+        });
+        
+        
+        
+    });
+    
+}
+
+module.exports = seedDB ;
